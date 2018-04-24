e249f2d99cf279dc1b5d909939cf5d256bf6b03c
Brian Karshick <briankar@buffalo.edu>
2018-04-24 16:32:47 -0400

    Placeholder implementation for syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -13,8 +13,22 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  uint32_t *args = ((uint32_t *) f->esp); /* Args are 32-bit words on stack. */
+
+  printf ("system call %d!\n", args[0]);
+
+  switch (args[0])
+  {
+    case SYS_WRITE: /* TODO */
+      printf ("write (%d, \"%s\", %u)\n", (int) args[3], (const char *) args[2], (unsigned) args[1]);
+      break;
+    case SYS_EXIT: /* TODO */
+      thread_exit ();
+      break;
+    default: /* TODO */
+      PANIC ("Unhandled system call!");
+      break;
+  }
 }

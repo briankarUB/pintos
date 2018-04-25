b7d91293d57ae3d73442e6514e88c2a53878fecb
Brian Karshick <briankar@buffalo.edu>
2018-04-24 23:18:45 -0400

    Rewrote tokenize. Added get_file_name.

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -18,23 +18,22 @@
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 
-#define ARGV_SIZE_LIMIT 512
-#define ARGC_LIMIT 32
+#define ARGV_SIZE_MAX 1024 /* Max length of command line string. */
+#define ARGC_MAX 64        /* Max number of arguments in command. */
 
-#define PUSH_POINTER(ESP, PTR, TYPE) \
-  (ESP) -= sizeof (TYPE); \
-  *((TYPE *) (ESP)) = (TYPE) (PTR);
+typedef void (*voidfp) (void);
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
-static void tokenize_command (const char *cmdline, void **esp);
+static void tokenize (const char *cmdline, char **esp);
+static void get_file_name (const char *cmdline, char *file_name);
 
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
 tid_t
-process_execute (const char *file_name) 
+process_execute (const char *file_name)
 {
   char *fn_copy;
   tid_t tid;
@@ -49,7 +48,7 @@ process_execute (const char *file_name)
   /* Create a new thread to execute FILE_NAME. */
   tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
-    palloc_free_page (fn_copy); 
+    palloc_free_page (fn_copy);
   return tid;
 }
 
@@ -71,7 +70,7 @@ start_process (void *file_name_)
 
   /* If load failed, quit. */
   palloc_free_page (file_name);
-  if (!success) 
+  if (!success)
     thread_exit ();
 
   /* Start the user process by simulating a return from an
@@ -94,7 +93,7 @@ start_process (void *file_name_)
    This function will be implemented in problem 2-2.  For now, it
    does nothing. */
 int
-process_wait (tid_t child_tid UNUSED) 
+process_wait (tid_t child_tid UNUSED)
 {
   while (1);
   NOT_REACHED ();
@@ -110,7 +109,7 @@ process_exit (void)
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
   pd = cur->pagedir;
-  if (pd != NULL) 
+  if (pd != NULL)
     {
       /* Correct ordering here is crucial.  We must set
          cur->pagedir to NULL before switching page directories,
@@ -215,8 +214,9 @@ static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
    and its initial stack pointer into *ESP.
    Returns true if successful, false otherwise. */
 bool
-load (const char *file_name, void (**eip) (void), void **esp) 
+load (const char *cmdline, void (**eip) (void), void **esp)
 {
+  char file_name[ARGV_SIZE_MAX];
   struct thread *t = thread_current ();
   struct Elf32_Ehdr ehdr;
   struct file *file = NULL;
@@ -224,18 +224,21 @@ load (const char *file_name, void (**eip) (void), void **esp)
   bool success = false;
   int i;
 
+  /* Get the file name into file_name. */
+  get_file_name (cmdline, file_name);
+
   /* Allocate and activate page directory. */
   t->pagedir = pagedir_create ();
-  if (t->pagedir == NULL) 
+  if (t->pagedir == NULL)
     goto done;
   process_activate ();
 
   /* Open executable file. */
-  file = filesys_open ("echo");
-  if (file == NULL) 
+  file = filesys_open (file_name);
+  if (file == NULL)
     {
       printf ("load: %s: open failed\n", file_name);
-      goto done; 
+      goto done;
     }
 
   /* Read and verify executable header. */
@@ -245,15 +248,15 @@ load (const char *file_name, void (**eip) (void), void **esp)
       || ehdr.e_machine != 3
       || ehdr.e_version != 1
       || ehdr.e_phentsize != sizeof (struct Elf32_Phdr)
-      || ehdr.e_phnum > 1024) 
+      || ehdr.e_phnum > 1024)
     {
       printf ("load: %s: error loading executable\n", file_name);
-      goto done; 
+      goto done;
     }
 
   /* Read program headers. */
   file_ofs = ehdr.e_phoff;
-  for (i = 0; i < ehdr.e_phnum; i++) 
+  for (i = 0; i < ehdr.e_phnum; i++)
     {
       struct Elf32_Phdr phdr;
 
@@ -264,7 +267,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
       if (file_read (file, &phdr, sizeof phdr) != sizeof phdr)
         goto done;
       file_ofs += sizeof phdr;
-      switch (phdr.p_type) 
+      switch (phdr.p_type)
         {
         case PT_NULL:
         case PT_NOTE:
@@ -278,7 +281,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
         case PT_SHLIB:
           goto done;
         case PT_LOAD:
-          if (validate_segment (&phdr, file)) 
+          if (validate_segment (&phdr, file))
             {
               bool writable = (phdr.p_flags & PF_W) != 0;
               uint32_t file_page = phdr.p_offset & ~PGMASK;
@@ -293,7 +296,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
                   zero_bytes = (ROUND_UP (page_offset + phdr.p_memsz, PGSIZE)
                                 - read_bytes);
                 }
-              else 
+              else
                 {
                   /* Entirely zero.
                      Don't read anything from disk. */
@@ -317,8 +320,9 @@ load (const char *file_name, void (**eip) (void), void **esp)
 
   /* Start address. */
   *eip = (void (*) (void)) ehdr.e_entry;
-  
-  tokenize_command (file_name, esp); /* TODO */
+
+  /* Tokenize and push arguments. */
+  tokenize (cmdline, (char **) esp);
 
   success = true;
 
@@ -335,24 +339,24 @@ static bool install_page (void *upage, void *kpage, bool writable);
 /* Checks whether PHDR describes a valid, loadable segment in
    FILE and returns true if so, false otherwise. */
 static bool
-validate_segment (const struct Elf32_Phdr *phdr, struct file *file) 
+validate_segment (const struct Elf32_Phdr *phdr, struct file *file)
 {
   /* p_offset and p_vaddr must have the same page offset. */
-  if ((phdr->p_offset & PGMASK) != (phdr->p_vaddr & PGMASK)) 
-    return false; 
+  if ((phdr->p_offset & PGMASK) != (phdr->p_vaddr & PGMASK))
+    return false;
 
   /* p_offset must point within FILE. */
-  if (phdr->p_offset > (Elf32_Off) file_length (file)) 
+  if (phdr->p_offset > (Elf32_Off) file_length (file))
     return false;
 
   /* p_memsz must be at least as big as p_filesz. */
-  if (phdr->p_memsz < phdr->p_filesz) 
-    return false; 
+  if (phdr->p_memsz < phdr->p_filesz)
+    return false;
 
   /* The segment must not be empty. */
   if (phdr->p_memsz == 0)
     return false;
-  
+
   /* The virtual memory region must both start and end within the
      user address space range. */
   if (!is_user_vaddr ((void *) phdr->p_vaddr))
@@ -393,14 +397,14 @@ validate_segment (const struct Elf32_Phdr *phdr, struct file *file)
    or disk read error occurs. */
 static bool
 load_segment (struct file *file, off_t ofs, uint8_t *upage,
-              uint32_t read_bytes, uint32_t zero_bytes, bool writable) 
+              uint32_t read_bytes, uint32_t zero_bytes, bool writable)
 {
   ASSERT ((read_bytes + zero_bytes) % PGSIZE == 0);
   ASSERT (pg_ofs (upage) == 0);
   ASSERT (ofs % PGSIZE == 0);
 
   file_seek (file, ofs);
-  while (read_bytes > 0 || zero_bytes > 0) 
+  while (read_bytes > 0 || zero_bytes > 0)
     {
       /* Calculate how to fill this page.
          We will read PAGE_READ_BYTES bytes from FILE
@@ -417,15 +421,15 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       if (file_read (file, kpage, page_read_bytes) != (int) page_read_bytes)
         {
           palloc_free_page (kpage);
-          return false; 
+          return false;
         }
       memset (kpage + page_read_bytes, 0, page_zero_bytes);
 
       /* Add the page to the process's address space. */
-      if (!install_page (upage, kpage, writable)) 
+      if (!install_page (upage, kpage, writable))
         {
           palloc_free_page (kpage);
-          return false; 
+          return false;
         }
 
       /* Advance. */
@@ -439,13 +443,13 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 /* Create a minimal stack by mapping a zeroed page at the top of
    user virtual memory. */
 static bool
-setup_stack (void **esp) 
+setup_stack (void **esp)
 {
   uint8_t *kpage;
   bool success = false;
 
   kpage = palloc_get_page (PAL_USER | PAL_ZERO);
-  if (kpage != NULL) 
+  if (kpage != NULL)
     {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
@@ -476,56 +480,70 @@ install_page (void *upage, void *kpage, bool writable)
           && pagedir_set_page (t->pagedir, upage, kpage, writable));
 }
 
+/* Tokenizes the command line and pushes arguments onto
+   the stack as per the 80x86 calling convention. Returns
+   the new value for the stack pointer. */
 static void
-tokenize_command (const char *cmdline, void **esp)
+tokenize (const char *cmdline, char **esp)
 {
-  char *esp_ = (char *) *esp; /* Working stack pointer. */
-
-  char buf[ARGV_SIZE_LIMIT]; /* Buffer to copy and tokenize cmdline. */
-  char *argv[ARGC_LIMIT]; /* Array of pointers to arguments on stack. */
-  char args[ARGV_SIZE_LIMIT];
+  /* For tokenizing */
+  char buf[ARGV_SIZE_MAX];
   char *token, *save_ptr;
-  size_t argc = 0, len = 0;
 
-  /* Copy cmdline into the above buffer. */
-  strlcpy (buf, cmdline, ARGV_SIZE_LIMIT);
+  /* Bookwork for keeping track of stack state. */
+  char *argv[ARGC_MAX]; /* Stores stack pointers to each argv. */
+  int argc = 0;
 
-  /* Tokenize the cmdline. */
+  /* Copy the command line string into our local buffer. */
+  strlcpy (buf, cmdline, ARGV_SIZE_MAX);
+
+  /* Push all arguments to the stack, including the executable name.
+     NOTE: This part is actually done in reverse order from the example
+     calling convention in Pintos 3.5.1, but it doesn't matter because
+     we make up for it by storing the argv pointers in the correct
+     order later on. From the program's point of view, there's nothing
+     different, assuming it doesn't manually inspect the stack. */
   for (token = strtok_r (buf, " ", &save_ptr); token != NULL;
        token = strtok_r (NULL, " ", &save_ptr))
     {
-      esp_ -= strlen (token) + 1;
-      len += strlen (token) + 1;
-      argv[argc++] = esp_;
+      *esp -= strlen (token) + 1; /* Decrement ESP to prepare for push. */
+      argv[argc++] = *esp; /* Store address of this string for later use. */
 
-      /* Copy this token onto the stack. */
-      // strlcpy (esp_, token, ARGV_SIZE_LIMIT);
+      /* Push this token (argument) onto the stack at ESP. */
+      strlcpy (*esp, token, ARGV_SIZE_MAX);
     }
 
-  /* Copy all arguments onto the stack at once. */
-  memcpy (esp_, argv, len);
+  /* Word-align ESP (to a multiple of 4). */
+  while (((uintptr_t) *esp) % 4 != 0)
+    --(*esp);
 
-  /* Word alignment. */
-  while ((uintptr_t) esp_ % 4 != 0)
-    --esp_;
+  /* Push argv pointers to the stack in order, including argv[argc] = NULL */
+  *esp -= (argc + 1) * sizeof (char *);
+  memcpy (*esp, &argv[0], (argc + 1) * sizeof (char *));
 
-  /* argv[argc] must be a null pointer. */
-  PUSH_POINTER (esp_, NULL, char *);
+  /* Push &argv[0] to the stack. */
+  *esp -= sizeof (char **);
+  *((char ***) (*esp)) = (char **) (*esp + sizeof (char **));
 
-  /* Push the argv pointers onto the stack. */
-  esp_ -= argc * sizeof (char *);
-  memcpy (esp_, argv, argc * sizeof (char *));
+  /* Push argc to the stack. */
+  *esp -= sizeof (int);
+  **esp = argc;
 
-  /* Push the pointer to argv[0] onto the stack. */
-  PUSH_POINTER (esp_, esp_ + sizeof (char **), char *);
+  /* Push the function's fake return address (RA) to the stack.
+     Explanation:
+       1. Decrement ESP by the size of a void function pointer.
+       2. Cast ESP to a pointer to a void function pointer.
+       3. Assign NULL to it. */
+  *esp -= sizeof (voidfp);
+  *((voidfp *) (*esp)) = NULL;
+}
 
-  /* Push a fake return address (RA) onto the stack. */
-  PUSH_POINTER (esp_, NULL, uintptr_t);
-
-  // size_t total = *esp - (void *) esp_;
-  size_t total = 52;
-  hex_dump (*esp - total, *esp - total, total, true);
-
-  /* Set the real stack pointer to where the working pointer is. */
-  *esp = (void *) esp_;
+/* Finds the first word in cmdline, then copies it into the buffer
+   at file_name. If cmdline is only one word, copies the entire
+   cmdline into file_name. */
+static void
+get_file_name (const char *cmdline, char *file_name)
+{
+  size_t len = strcspn (cmdline, " ") + 1;
+  strlcpy (file_name, cmdline, len);
 }

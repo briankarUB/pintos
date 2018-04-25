534fd1772e3ac31d20558257586c37c227fbac7a
Brian Karshick <briankar@buffalo.edu>
2018-04-25 03:36:34 -0400

    No more black magic

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,61 +5,38 @@
 #include "threads/thread.h"
 #include "devices/shutdown.h"
 
-#define SYSCALL(NUM, RETURNS) \
-  static uint32_t syscall_##NUM##__ (uint32_t *args UNUSED, size_t idx__ UNUSED)
+typedef int pid_t;
 
-#define REGISTER(NUM, RETURNS) \
-  DISPATCH_TABLE[NUM].fn = syscall_##NUM##__; \
-  DISPATCH_TABLE[NUM].returns = RETURNS
+#define call0_nr(NAME) NAME ()
+#define call1_nr(NAME, T1) NAME ((T1) args[1])
+#define call2_nr(NAME, T1, T2) NAME ((T1) args[1], (T2) args[2])
+#define call3_nr(NAME, T1, T2, T3) NAME ((T1) args[1], (T2) args[2], (T3) args[3])
 
-#define ARG(TYPE, NAME) TYPE NAME = (TYPE) args[++idx__]
-
-struct strategy
-  {
-    uint32_t (*fn) (uint32_t *, size_t);
-    bool returns;
-  };
-
-static struct strategy DISPATCH_TABLE[20];
+#define call0(NAME) (uint32_t) call0_nr (NAME)
+#define call1(NAME, T1) (uint32_t) call1_nr (NAME, T1)
+#define call2(NAME, T1, T2) (uint32_t) call2_nr (NAME, T1, T2)
+#define call3(NAME, T1, T2, T3) (uint32_t) call3_nr (NAME, T1, T2, T3)
 
 static void syscall_handler (struct intr_frame *);
 
-SYSCALL (SYS_HALT, false)
-{
-  shutdown_power_off ();
-}
-
-SYSCALL (SYS_EXIT, false) /* TODO: Wake up waiting threads? */
-{
-  ARG (int, status);
-
-  printf ("%s: exit(%d)\n", thread_current ()->name, status);
-  thread_exit ();
-}
-
-SYSCALL (SYS_WRITE, true) /* TODO: Handle other fd besides 1 */
-{
-  ARG (int, fd);
-  ARG (const char *, buffer);
-  ARG (unsigned, size);
-
-  switch (fd)
-  {
-    case 1:
-      putbuf (buffer, size);
-      return size;
-    default:
-      PANIC ("Unknown fd for SYS_WRITE call");
-  }
-}
+/* Syscalls */
+static void halt (void);
+static void exit (int status);
+static pid_t exec (const char *cmd_line);
+static int wait (pid_t pid);
+static bool create (const char *file, unsigned initial_size);
+static bool remove (const char *file);
+static int open (const char *file);
+static int filesize (int fd);
+static int read (int fd, void *buffer, unsigned size);
+static int write (int fd, const void *buffer, unsigned size);
+static void seek (int fd, unsigned position);
+static unsigned tell (int fd);
+static void close (int fd);
 
 void
 syscall_init (void)
 {
-  REGISTER (SYS_HALT, false);
-  REGISTER (SYS_EXIT, false);
-  REGISTER (SYS_WRITE, true);
-
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
@@ -67,10 +44,129 @@ static void
 syscall_handler (struct intr_frame *f)
 {
   uint32_t *args = (uint32_t *) f->esp;
-  struct strategy strat = DISPATCH_TABLE[args[0]];
 
-  if (strat.returns)
-    f->eax = strat.fn (args, 0);
-  else
-    strat.fn (args, 0);
+  /* Dispatch based on syscall number.
+     TODO: Maybe there's a more modular(?) way to do this, but who cares? */
+  switch (args[0])
+  {
+    case SYS_HALT:
+      call0_nr (halt);
+      return;
+    case SYS_EXIT:
+      call1_nr (exit, int);
+      return;
+    case SYS_EXEC:
+      f->eax = call1 (exec, const char *);
+      return;
+    case SYS_WAIT:
+      f->eax = call1 (wait, pid_t);
+      return;
+    case SYS_CREATE:
+      f->eax = call2 (create, const char *, unsigned);
+      return;
+    case SYS_REMOVE:
+      f->eax = call1 (remove, const char *);
+      return;
+    case SYS_OPEN:
+      f->eax = call1 (open, const char *);
+      return;
+    case SYS_FILESIZE:
+      f->eax = call1 (filesize, int);
+      return;
+    case SYS_READ:
+      f->eax = call3 (read, int, void *, unsigned);
+      return;
+    case SYS_WRITE:
+      f->eax = call3 (write, int, const void *, unsigned);
+      return;
+    case SYS_SEEK:
+      call2_nr (seek, int, unsigned);
+      return;
+    case SYS_TELL:
+      f->eax = call1 (tell, int);
+      return;
+    case SYS_CLOSE:
+      call1_nr (close, int);
+      return;
+    default:
+      PANIC ("Non-implemented syscall");
+      return;
+  }
+}
+
+static void
+halt (void)
+{
+  shutdown_power_off ();
+}
+
+static void
+exit (int status) /* TODO: Make sure this plays nicely with wait. */
+{
+  printf ("%s: exit(%d)\n", thread_current ()->name, status);
+  thread_exit ();
+}
+
+static int
+write (int fd, const void *buffer, unsigned size)
+{
+  if (fd == 1)
+  {
+    putbuf (buffer, size);
+      return size;
+  }
+  else /* TODO: Implement for fd != 1 */
+  {
+    PANIC ("Unknown fd for SYS_WRITE call");
+  }
+}
+
+static pid_t exec (const char *cmd_line)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static int wait (pid_t pid)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static bool create (const char *file, unsigned initial_size)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static bool remove (const char *file)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static int open (const char *file)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static int filesize (int fd)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static int read (int fd, void *buffer, unsigned size)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static void seek (int fd, unsigned position)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static unsigned tell (int fd)
+{
+  PANIC ("Not implemented"); /* TODO */
+}
+
+static void close (int fd)
+{
+  PANIC ("Not implemented"); /* TODO */
 }

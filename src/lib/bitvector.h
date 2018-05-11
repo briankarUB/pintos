9213fb1b049f01400a5d646306df6bd7874f5e41
Brian Karshick <briankar@buffalo.edu>
2018-05-11 15:20:05 -0400

    Fix make error when using bitvector

--- src/lib/bitvector.h
+++ src/lib/bitvector.h
@@ -1,15 +1,17 @@
 #ifndef __LIB_BITVECTOR_H
 #define __LIB_BITVECTOR_H
 
-#define BITVECTOR_SIZE 8
-
 #include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#define BITVECTOR_SIZE 8
 
 typedef uint8_t bv_cnt_t;
 
 struct bitvector
   {
-    bv_cnt_t val[BITVECTOR_SIZE]; 
+    bv_cnt_t value[BITVECTOR_SIZE]; 
   };
 
 void bitvector_init (struct bitvector *vec);
